1d8dfda99ba60ef18bb737b54f46c9c3eb8dfbd8
Nathan B <nathanbommezijn@gmail.com>
2025-12-07 18:11:17 +0100

    WIP: Astro + react + shadcn (tailwind)

---
+++ apps/frontend/astro.config.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+
+import react from "@astrojs/react";
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  // Enable React to support React JSX components.
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwind(), tailwindcss()],
+  },
+});
---
+++ apps/frontend/src/components/Counter.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { Button } from "../components/ui/button";
+// import "./Counter.css";
+
+export default function Counter({
+  children,
+  count: initialCount,
+}: {
+  children: JSX.Element;
+  count: number;
+}) {
+  const [count, setCount] = useState(initialCount);
+  const add = () => setCount((i) => i + 1);
+  const subtract = () => setCount((i) => i - 1);
+
+  return (
+    <>
+      <div className="counter">
+        <Button variant="outline" onClick={subtract}>
+          -
+        </Button>
+        <pre>{count}</pre>
+        <button onClick={add}>+</button>
+      </div>
+      <div className="counter-message">{children}</div>
+    </>
+  );
+}
